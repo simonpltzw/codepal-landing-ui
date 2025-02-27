3b7d7308e59625f7cc4c4f78322e6a8001b24e89
simonpltzw <github.karma467@passmail.net>
2025-02-27 22:32:22 +0100

    fix(config): New landing ui repository url

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "palzone.de",
   domain: "palzone.de",
-  version: "v0.3.0",
+  version: "v0.3.1",
   description: "Explore my portfolio showcasing a collection of inspiring projects that bring ideas to life.",
   mail: "info@palzone.de",
   mainNav: [
@@ -26,7 +26,7 @@ export const siteConfig = {
     github_project_ffischbach_events: "https://github.com/simonpltzw/ffischbach-events",
     github_project_ffischbach_events_rust: "https://github.com/simonpltzw/ffischbach-events-api-rs",
     github_project_stringcompare_api: "https://github.com/simonpltzw/stringcompare-api",
-    github_project_landing_ui: "https://github.com/simonpltzw/codepal-landing-ui",
+    github_project_landing_ui: "https://github.com/simonpltzw/landing-ui",
     github_project_todo_ui: "https://github.com/simonpltzw/todo-ui",
     github_project_todo_api: "https://github.com/simonpltzw/todo-api"
   },
